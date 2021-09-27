33f31dfa349d800e84dc0d79cc8226aff78fc337
Tom <t.p.kellett@gmail.com>
2021-09-27 14:51:42 +0100

    Adding balance, top_up methods and limit on amount you can top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,14 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
+  LIMIT = 90
+
   def initialize
     @balance = 0
+    @limit = LIMIT
+  end
+
+  def top_up(amount)
+    raise "You cannot top_up over the limit of #{@limit}" if @balance + amount > @limit
+    @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,13 @@ describe Oystercard do
   it "defaults to a balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  it "adds money to the card balance" do 
+    subject.top_up(5)
+    expect(subject.balance).to eq(5)
+  end
+
+  it 'prevents you from topping up beyond the maximum balance' do
+    expect { subject.top_up(subject.limit+1) }.to raise_error "You cannot top_up over the limit of #{subject.limit}"
+  end
 end
